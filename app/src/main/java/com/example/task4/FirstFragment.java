35beea7cdbe35e451cf82666682a68e6c37174a0
Taileduser <aalex.03@mail.ru>
2023-03-10 20:16:18 +0300

    button actions added

--- app/src/main/java/com/example/task4/FirstFragment.java
+++ app/src/main/java/com/example/task4/FirstFragment.java
@@ -3,16 +3,18 @@ package com.example.task4;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.task4.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
-    public FragmentFirstBinding binding;
+    private FragmentFirstBinding binding;
 
 
     @Override
@@ -21,8 +23,19 @@ public class FirstFragment extends Fragment {
         binding = FragmentFirstBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
 
+        Button leftButton = binding.buttonLeft;
+        Button leftRight = binding.buttonRight;
+
+        
 
 
         return binding.getRoot();
+    }
+
+    private void switchFrag(FragmentContainerView fcv, Fragment fragment) {
+        requireActivity().getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(fcv.getId(), fragment)
+                .commit();
     }
 }
--- app/src/main/java/com/example/task4/MainActivity.java
+++ app/src/main/java/com/example/task4/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.task4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
 import android.view.Window;
 
+import com.example.task4.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        FragmentContainerView containerView = binding.containerView;
+
+        FirstFragment firstFragment = new FirstFragment();
+        switchFrag(containerView, firstFragment);
+
+    }
+
+    private void switchFrag(FragmentContainerView fcv, Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(fcv.getId(), fragment)
+                .commit();
     }
 }
