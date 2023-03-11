ea8877cf75935e9647a0f7f6e064948131f6823c
Taileduser <aalex.03@mail.ru>
2023-03-11 03:22:01 +0300

    added click listener in RecyclerView

--- app/src/main/java/com/example/task4/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/task4/MyRecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.example.task4;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,6 +38,13 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         if (item.getImageId() != 0) {
             holder.imageView.setImageResource(item.getImageId());
         }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), item.getText(), Toast.LENGTH_SHORT).show();
+                Log.i("RecyclerAdapter", item.getText());
+            }
+        });
     }
 
     @Override
